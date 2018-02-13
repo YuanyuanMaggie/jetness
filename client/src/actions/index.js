9203f4e26952ec429be06d5c1aad21a3d400b764
YuanYuan Li <yuanyuan.li@yli-m.jet.local>
2018-02-13 14:36:47 -0500

    update redux

---
+++ client/src/actions/index.js
@@ -0,0 +1,5 @@
+import { FETCH_USER } from './types';
+
+export const fetchUser = (data) => {
+    return {type: FETCH_USER, payload: data});
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducers, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, 
+    document.querySelector('#root')
+);
