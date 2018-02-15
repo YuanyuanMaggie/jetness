4614e3bab02c36ffe07463fe6f577b789d8c8f6e
Bo Huang <bo.huang@bhuang-m.jet.local>
2018-02-14 21:27:03 -0500

    result page is done

--- client/src/components/Result/index.js
+++ client/src/components/Result/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Slider from 'react-slick';
+import { Link } from 'react-router-dom';
 
 import MealItem from '../MealItem';
 import WearItem from '../WearItem';
 import SupplementItem from '../SupplementItem';
+import InputPlaceHolder from './InputPlaceHolder';
+import Goal from './Goal';
 
 import './index.css';
 
@@ -37,7 +40,60 @@ const ResultHeader = () => (
 class ResultForm extends React.Component {
   render() {
     return (
-      <div></div>
+      <div className="result-form">
+        <video width="400" height="320" controls loop>
+          <source src="images/3d-body.mp4" type="video/mp4"></source>
+        </video>
+
+        <div className="form">
+          <div className="top">
+            <div className="title">TOP (INCH)</div>
+            <div className="top-result">
+              <div className="top-header">
+                <span>Shoulder</span>
+                <span>Sleeve</span>
+                <span>Chest</span>
+                <span>Waist Middle</span>
+                <span>Waist Bottom</span>
+              </div>
+              <div className="top-body">
+                <span>16.38</span>
+                <span>21.92</span>
+                <span>36.60</span>
+                <span>29.48</span>
+                <span>30.48</span>
+              </div>
+            </div>
+            <div className="top-size">
+              <div>Your Top Size</div><InputPlaceHolder placeholder='M' /><InputPlaceHolder placeholder='US6' />
+            </div>
+          </div>
+
+          <div className="bottom">
+            <div className="title">BOTTOM</div>
+            <div className="bottom-result">
+              <div className="bottom-header">
+                <span>Hips</span>
+                <span>Top of Thigh</span>
+                <span>Around Calf</span>
+                <span>Size Knee</span>
+                <span>Below Buttock to Heal</span>
+              </div>
+              <div className="bottom-body">
+                <span>37.75</span>
+                <span>22.52</span>
+                <span>14.57</span>
+                <span>24.70</span>
+                <span>30%</span>
+              </div>
+            </div>
+            <div className="bottom-size">
+              <div>Your Bottom Size</div><InputPlaceHolder placeholder='M' />
+              <div>Shoes Size</div><InputPlaceHolder placeholder='8' />
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 };
@@ -45,25 +101,62 @@ class ResultForm extends React.Component {
 class SetDate extends React.Component {
   render() {
     return (
-      <div></div>
+      <div className="set-date">
+        <div className="title">Set a date for your goal</div>
+        <div className="day">
+          <div>Day</div><InputPlaceHolder placeholder='M' /><div className="icon-caret-down"></div>
+        </div>
+        <div className="month">
+          <div>Month</div><InputPlaceHolder placeholder='M' /><div className="icon-caret-down"></div>
+        </div>
+      </div>
     );
   }
 };
 
 class SetGoal extends React.Component {
   render() {
+    let goals = [
+      { name: 'Feel Better', src: 'images/plan1.png', weight: false },
+      { name: 'Loose Weight', src: 'images/plan2.png', weight: true },
+      { name: 'Gain Weight', src: 'images/plan3-.png', weight: true },
+      { name: 'Boost Your Energy', src: 'images/plan4.png', weight: false },
+      { name: 'Immunity', src: 'images/plan5.png', weight: false },
+      { name: 'Recomp', src: 'images/plan6.png', weight: false },
+    ];
+
     return (
-      <div></div>
+      <div className="set-goal">
+        <div className="title">Set your goals</div>
+        <div className="goals">
+          {
+            goals.map((goal, i) => 
+              <div key={i} className="goal-wrwapper">
+                <Goal name={goal.name} src={goal.src} weight={goal.weight} />
+              </div>
+            )
+          }
+        </div>
+      </div>
     );
   }
 };
 
+class SetGoalButton extends React.Component {
+  render() {
+    return (
+      <Link to="/recommendations" className="set-goal-button">Set The Goal</Link>
+    );
+  }
+}
+
 const Result = () => (
     <div className="result">
         <ResultHeader />
-        <ResultForm/>
-        <SetDate/>
-        <SetGoal/>
+        <ResultForm />
+        <SetDate />
+        <SetGoal />
+        <SetGoalButton />
     </div>
 )
 
